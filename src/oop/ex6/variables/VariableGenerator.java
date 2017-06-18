b9e257c95ac0a6697674ceb43d5af80fcbf685cf
xelanos <Tomeror1478>
2017-06-18 10:46:16 +0300

    fixed generator and javadoc

--- src/oop/ex6/variables/VariableGenerator.java
+++ src/oop/ex6/variables/VariableGenerator.java
@@ -21,18 +21,26 @@ public class VariableGenerator {
         return instance;
     }
 
-    public Variable makeVariable(String varType, String varValue, String varName, String varModifiyer)
+    /**
+     * @param varType variable desired type.
+     * @param varValue variable desired value.
+     * @param varName variable name.
+     * @param varModifier variable modifier.
+     * @return the desired variable.
+     * @throws CodeException thrown when type is not supported or value doesn't matches type.
+     */
+    public Variable makeVariable(String varType, String varValue, String varName, String varModifier)
             throws CodeException {
         switch (varType){
-            case "int": return new Int(varValue,varName,varModifiyer);
+            case "int": return new Int(varValue,varName,varModifier);
 
-            case "boolean": return new Boolean(varValue,varName,varModifiyer);
+            case "boolean": return new Boolean(varValue,varName,varModifier);
 
-            case "char": return new Char(varValue,varType,varName);
+            case "char": return new Char(varValue,varName,varModifier);
 
-            case "double": return new Double(varValue, varType,varName);
+            case "double": return new Double(varValue,varName,varModifier);
 
-            case "String": return new VarString(varValue, varType, varName);
+            case "String": return new VarString(varValue,varName,varModifier);
 
             default: throw new CodeException("Bad variable type");
         }
