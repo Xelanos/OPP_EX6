21e9e4fa08f9afa1179b253d21e4d26ab34ca6e5
yanirelfassy <yanir.elfassy@mail.huji.ac.il>
2017-06-19 09:55:11 +0300

    parser start

--- src/oop/ex6/main/Parser.java
+++ src/oop/ex6/main/Parser.java
@@ -1,6 +1,6 @@
 package oop.ex6.main;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by OrMiz on 12/06/2017.
@@ -8,6 +8,12 @@ import java.io.File;
 public class Parser {
 
     void processesLines(File sJavaFile) throws CodeException{
+        try(BufferedReader lineReader = new BufferedReader(new FileReader(sJavaFile.getPath()))) {
+
+        }
+        catch (IOException badFile){
+            throw new CodeException("Bad File");
+        }
     }
 
 }
