f59d096dc7606f9e7accc47822fa12a0006c80db
yanirelfassy <yanir.elfassy@mail.huji.ac.il>
2017-06-20 11:20:34 +0300

    add methods to regex worker

--- src/oop/ex6/main/RegexWorker.java
+++ src/oop/ex6/main/RegexWorker.java
@@ -3,19 +3,19 @@ package oop.ex6.main;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by Yanir on 19/06/2017.
- */
+
 public class RegexWorker {
 
-    public static final String COMMENT = "^[/]+[/]";
-    public static final String BLANK_ROW = "^\\s*$";
-    public static final String END_WITH_OPEN_BARKETS = "[(]$";
-    public static final String END_WITH_EQUAL = "=$";
-    public static final String FIRST_WORD =
+    private static final String COMMENT = "^[/]+[/]";
+    private static final String BLANK_ROW = "^\\s*$";
+    private static final String END_WITH_OPEN_BARKETS = "[(]$";
+    private static final String END_WITH_EQUAL = "=$";
+    private static final String FIRST_WORD =
             "(\\w*)+[(]|^\\s*(\\w*)+[(]|(\\w*)\\s[=]|^\\s*(\\w*)\\s[=]|^\\s*(\\w*)|(\\w*)";
     public static final String IS_ALL_WORD_REGEX = "^\\w+";
-    public static final String SCOPE_CLOSING = "\\w*}|\\s*\\w*}";
+    private static final String SCOPE_CLOSING = "\\w*}|\\s*\\w*}";
+    private static final String RETURN = "(return;)";
+
 
     static String getFirstWord(String line) throws CodeException{
         Pattern firstWordPattern = Pattern.compile(FIRST_WORD);
@@ -28,42 +28,46 @@ public class RegexWorker {
         }
     }
 
-    static String cleanWord(String word){
+    private static String cleanWord(String word){
         Pattern cleanPattern = Pattern.compile("[\\w]+");
         Matcher result = cleanPattern.matcher(word);
         return result.group(1);
     }
 
     static boolean isMethodDeclaration(String startingWord){
-        String cleanWord = RegexWorker.cleanWord(startingWord);
+        String cleanWord = cleanWord(startingWord);
         return cleanWord.equals("void");
     }
 
     static boolean isConditionDeclaration(String startingWord){
-        String cleanWord = RegexWorker.cleanWord(startingWord);
+        String cleanWord = cleanWord(startingWord);
         return cleanWord.equals("if");
     }
 
     static boolean isLoopDeclaration(String startingWord){
-        String cleanWord = RegexWorker.cleanWord(startingWord);
+        String cleanWord = cleanWord(startingWord);
         return cleanWord.equals("while");
     }
 
     static boolean isCallingMethod(String startingWord){
-        String cleanWord = RegexWorker.cleanWord(startingWord);
-        return cleanWord.matches(RegexWorker.END_WITH_OPEN_BARKETS);
+        String cleanWord = cleanWord(startingWord);
+        return cleanWord.matches(END_WITH_OPEN_BARKETS);
     }
 
     static boolean isCallingVar(String startingWord){
-        String cleanWord = RegexWorker.cleanWord(startingWord);
-        return cleanWord.matches(RegexWorker.END_WITH_EQUAL);
+        String cleanWord = cleanWord(startingWord);
+        return cleanWord.matches(END_WITH_EQUAL);
     }
 
     static boolean isCommentOrBlank(String line){
-        return ((line.matches(RegexWorker.COMMENT) || line.matches(RegexWorker.BLANK_ROW)));
+        return ((line.matches(COMMENT) || line.matches(BLANK_ROW)));
     }
 
     static boolean isClosingScope(String line){
-        return ((line.matches(RegexWorker.SCOPE_CLOSING)));
+        return ((line.matches(SCOPE_CLOSING)));
+    }
+
+    static boolean isReturn(String line){
+        return line.matches(RETURN);
     }
 }
